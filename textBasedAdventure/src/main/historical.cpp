db738fc2db8bc463ba0a2379a3e8b5082323d736
nashale <111540911+nashale@users.noreply.github.com>
2023-04-10 11:27:40 -0600

    Add files via upload

---
+++ textBasedAdventure/src/main/historical.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "historical.h"
+#include <fstream>
+
+
+void Historical::enter() {
+  // Input a Historical theme script to get information and choices from
+}
+void Historical::moveTo() {
+  // set variable to greater to move forward in script
+}
+void Historical::moveBack() {
+  // set variable to less than to backwards in script
+}
+void Historical::talk() {
+  // Input a Historical theme script to get information and choices from a npc
+}
+void Historical::use() {
+  // Use an item in the user's inventory
+}
+void Historical::pickUp() {
+  // User interacts or picks up an item that is available
+}
+
+
