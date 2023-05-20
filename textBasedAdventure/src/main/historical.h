33d3ed242d1a7fd4978215474f5d41fd43dbb96a
Jax Jameson <jaxsjameson@gmail.com>
2023-05-19 22:38:46 -0600

    fixed

--- textBasedAdventure/src/main/historical.h
+++ textBasedAdventure/src/main/historical.h
@@ -1,21 +1,18 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 class Historical {
 public:
-
-int f;
+  int f;
   int h;
   int v = 0;
   int mb;
   void enter(); // used at the beginning of the game, when the Victorian theme
                 // is selected
-  void moveTo(int m);   // given choices of where to move at user's current time
-  void moveBack(); // Just like moveTo
-  void talk();     // talk to the king
+  void moveTo(int m); // given choices of where to move at user's current time
+  void moveBack();    // Just like moveTo
+  void talk();        // talk to the king
   void help();
   void pickUp();
-void use ();
-
-
+  void use();
 };
